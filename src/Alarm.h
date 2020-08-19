02a01414ee8d40abd5faa360a5bec21eb3080b8f
Евгений А. Панкратов <EPankratov@Lissib.local>
2020-08-19 10:28:34 +0700

    Удалил ненужный код

--- src/Alarm.h
+++ src/Alarm.h
@@ -8,8 +8,7 @@ private:
 public:
     //Авария лампы
   int AlarmLamp(bool ActiveAlarmLamp){        
-        //lcd.locate(13,0);        
-        //lcd.printf("<!>");
+
         if (ActiveAlarmLamp)
         {
             return(1);
